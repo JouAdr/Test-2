cb380eb2c200bfcbeb65235562eb26fbbded7101
Jou Adr <joaryandrian@gmail.com>
2021-09-24 10:29:01 +0100

    Jou | sum of internet consommation

--- internetconsommation/internetconsommationapp/views.py
+++ internetconsommation/internetconsommationapp/views.py
@@ -1,34 +1,32 @@
 from django.shortcuts import render
 from .models import InternetConsumption
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def search(request):
     consommations = InternetConsumption.objects.all()
-    consommations_totals_sorted = list()
-    consommations_total = list()
+    consommations_totals_sorted = []
+    consommations_total = []
+    consommations_user_name = []
 
     for consommation in consommations: 
         consommations_total.append([consommation.user.username, (round(((consommation.upload + consommation.download)*0.000001), 2))])
+        consommations_user_name.append(consommation.user.username)
 
-    # for user, consommation in consommations_total:
-    #     if consommations_total.count(user) > 1:
-    #         consommations_totals_sorted.append([user, sum(consommation)])
-    #     else:
-    #         consommations_totals_sorted.append([user, consommation])
+    consommations_user = list(set(consommations_user_name))
 
-
-    for i in range(len(consommations_total)):
-        if consommations_total[i-1][0] == consommations_total[i][0]:
-            consommations_totals_sorted.append([consommations_total[i][0], (consommations_total[i][1] + consommations_total[i+1][1])])
-        else:
-            consommations_totals_sorted.append([consommations_total[i][0], consommations_total[i][1]])
-
-            
     #defines what happens when there is a GET request
     if request.method == "GET":
         username = request.GET.get("q")
-        return render(request,'home.html', { 'username' : username ,'consommations_totals_sorted':consommations_totals_sorted})
+        result = 0
+        for user, consommation in consommations_total:
+            if user == username:
+                result = round((result + consommation ),2) 
+                consommations_totals_sorted.append([user, result])
+            else:
+                consommations_totals_sorted.append([user, consommation])
+        return render(request,'home.html', { 'username' : username ,'consommations_totals_sorted':consommations_totals_sorted, 'consommations_user':consommations_user})
 
     #defines what happens when there is a POST request
     else:   
